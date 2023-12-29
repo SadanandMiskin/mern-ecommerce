4570fb62efd0e4984820c11819b9c0b58250f58b
Sadanand Miskin <miskinsadanand@gmail.com>
2023-12-30 01:12:03 +0530

    initial: add and display products

---
+++ routes/getproducts.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Product from '../models/product.js';
+
+const router = express.Router();
+
+
+
+router.get('/',  async (req, res) => {
+ 
+    try{
+       const products =  await Product.find()
+       const productList = [...products]
+       console.log(productList)
+       res.json(
+        productList
+       ).status(200)
+    }
+    catch(err) {
+        console.log(err)
+    }
+});
+
+export default router;
---
+++ server.js
@@ -0,0 +1,31 @@
+
+import express from "express"
+import path from "path"
+import bodyParser from "body-parser";
+const app = express() 
+
+const __dirname = path.resolve();
+
+
+import {db} from "./config/db.js"
+import productRoutes from "./routes/product.js"
+import productList from "./routes/getproducts.js"
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use('/uploads', express.static('uploads'))
+app.use(express.json());
+
+app.use('/addproduct', productRoutes)
+app.use('/getproducts', productList)
+
+app.listen(3000, async()=>{
+  try{
+    await db() 
+    console.log("server listening") 
+  }
+  catch(err) {
+    console.log(err)
+  }
+})
