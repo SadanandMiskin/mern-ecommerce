50b5bdcac89e9e93bf146e8f62bc8b39021c7108
Sadanand Miskin <miskinsadanand@gmail.com>
2024-01-14 12:43:57 +0530

    fix conflict

--- Backend/routes/customerLogin.js
+++ Backend/routes/customerLogin.js
@@ -4,10 +4,12 @@ import customer from '../models/customer.js'
 import passport from 'passport'
 
 const router = express.Router()
-
+ 
 // router.get('/' , (req,res)=>{
 //     res.render('login')
 // })
+
+
 
 router.post('/' ,passport.authenticate("local"), async(req,res) =>{
     res.json('Authenticated')
